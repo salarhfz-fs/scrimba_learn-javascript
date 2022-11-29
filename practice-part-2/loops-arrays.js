baa89f4d95b521d6d3f89062b410cafc162854b4
Salar Hafezi <salar.hfz.work@gmail.com>
2022-11-29 09:26:01 +0330

    add loops-arrays

---
+++ practice-part-2/loops-arrays.js
@@ -0,0 +1,7 @@
+let largeCountries = ["China","India","USA","Indonesia","Pakistan"];
+
+console.log('The 5 largest countries in the world:');
+
+for (let i = 0; i < largeCountries.length; i++) {
+    console.log('- ' + largeCountries[i]);
+}
