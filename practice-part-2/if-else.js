871a9dad85023c45e64d6a0abe3f88ced63d8371
Salar Hafezi <salar.hfz.work@gmail.com>
2022-11-29 09:25:26 +0330

    add if-else

---
+++ practice-part-2/if-else.js
@@ -0,0 +1,7 @@
+let age = 5;
+
+if (age < 6) console.log('free');
+else if (age < 18) console.log('child discount');
+else if (age < 27) console.log('student discount');
+else if (age < 67) console.log('full price');
+else console.log('senior citizen discount');
