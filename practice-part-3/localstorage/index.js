24918574afa52f10e7f7d0d8b66f21357886ebc1
Salar Hafezi <salar.hfz.work@gmail.com>
2022-11-30 14:24:44 +0330

    add localstorage

---
+++ practice-part-3/localstorage/index.js
@@ -0,0 +1,8 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+localStorage.setItem('key', 'value');
+const value = localStorage.getItem('key');
+
+console.log(`Here is the value: ${value}.`);
